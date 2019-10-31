f7bc0bd19e2cb24fbd85d7f811f960a96790982a
Ryan Harris <ragobash@gmail.com>
2019-10-31 17:47:39 -0500

    Updates

---
+++ precache-manifest.b288df57a5fda6146b0dd6a16b586812.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16d05ea2ecd3850781c365d14189a745",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "f2d6b53dbe8d82824f7b",
+    "url": "/clicky-game/static/css/main.1460377a.chunk.css"
+  },
+  {
+    "revision": "858337689f8457637e8e",
+    "url": "/clicky-game/static/js/2.05bcf8b3.chunk.js"
+  },
+  {
+    "revision": "f2d6b53dbe8d82824f7b",
+    "url": "/clicky-game/static/js/main.d08e3b69.chunk.js"
+  },
+  {
+    "revision": "9e63f44ec9a3dfc4c648",
+    "url": "/clicky-game/static/js/runtime-main.742e30a5.js"
+  }
+]);
